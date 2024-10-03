8e4ffa93e21935096b09d7c5573befa303c312d8
Joshua Maserow <joshua@softwarecreations.co.za>
2024-10-03 12:40:13 +0200

    'Renamed ambiguous path to filePath. Added TS declarations and bumped version.'

---
+++ main.d.ts
@@ -0,0 +1,11 @@
+declare module 'esc-file-replace-substring-between-comments' {
+  export const fileReplaceSubstringBetweenComments: (
+    filePath   : string, 
+    purpose    : string, 
+    newContents: string, 
+    ifNotFound?: 'append', 
+    changedLogF?  : (message: string) => void | null, 
+    notFoundLogF? : (message: string) => void | null, 
+    unchangedLogF?: (message: string) => void | null
+  ) => boolean;
+}
--- main.mjs
+++ main.mjs
@@ -1,9 +1,9 @@
 'use strict';
 import fileReplaceSubstring from 'esc-file-replace-substring';
 
-export const fileReplaceSubstringBetweenComments = (path, purpose, newContents, ifNotFound, changedLogF, notFoundLogF, unchangedLogF) => {
+export const fileReplaceSubstringBetweenComments = (filePath, purpose, newContents, ifNotFound, changedLogF, notFoundLogF, unchangedLogF) => {
 	const startComment=`#${purpose} starts`, endComment=`#${purpose} ends`, searchRegex=RegExp(`${startComment}[\\w\\W]*?${endComment}\n`);
-	return fileReplaceSubstring(path, searchRegex, `${startComment}\n${newContents}\n${endComment}\n`, ifNotFound, changedLogF, notFoundLogF, unchangedLogF);
+	return fileReplaceSubstring(filePath, searchRegex, `${startComment}\n${newContents}\n${endComment}\n`, ifNotFound, changedLogF, notFoundLogF, unchangedLogF);
 };
 
 export default fileReplaceSubstringBetweenComments;
